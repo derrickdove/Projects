364c4fc982be14ea81875509fab5d5f32d39bd30
Derrick Dove <122322054+derrickdove@users.noreply.github.com>
2023-03-29 15:17:53 -0400

    Mid Quiz push

--- Quiz/src/main/java/org/example/Quiz.java
+++ Quiz/src/main/java/org/example/Quiz.java
@@ -12,8 +12,9 @@ public class Quiz implements ActionListener{
                                     {"Test1", "Test2", "Test3", "Test4"},
                                     {"Test1", "Test2", "Test3", "Test4"},
                         }; // contains quiz options
-
-   char answer;
+    char[] answers = {
+            'A','B','C'
+                     };
    char guess;
    int index;
    int correctGuess = 0;
@@ -21,7 +22,7 @@ public class Quiz implements ActionListener{
    int result;
    int seconds = 10;
 
-   JFrame frame = new JFrame();
+   JFrame frame = new JFrame(); // creates an object of the JFrame called frame
    JTextField  textField = new JTextField();
    JTextArea textArea = new JTextArea();
    JButton buttonA = new JButton();
@@ -39,26 +40,116 @@ public class Quiz implements ActionListener{
 
 
 
-   char[] answer = {
-                       'A','B','C'
-                  };
+
 
 
     public Quiz() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(650,650);
+        frame.getContentPane().setBackground(new Color(220,223,228)); // sets background color
+        frame.setLayout(null);
+        frame.setResizable(false);
+
+        textField.setBounds(0,0,650,50);
+        textField.setBackground(new Color(25,25,25));
+        textField.setForeground(new Color(25,100,0));
+        textField.setFont(new Font("Ink Free", Font.BOLD,30));
+        textField.setBorder(BorderFactory.createBevelBorder(1));
+        textField.setHorizontalAlignment(JTextField.CENTER);
+        textField.setEditable(false);
+
+
+        textArea.setBounds(0,50,650,50);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setBackground(new Color(25,25,25));
+        textArea.setForeground(new Color(25,255,0));
+        textArea.setFont(new Font("MV Boli", Font.BOLD,25));
+        textArea.setBorder(BorderFactory.createBevelBorder(1));
+        textArea.setEditable(false);
+        textArea.setText("Same Text ");
+
+
+        buttonA.setBounds(0,100,100,100);
+        buttonA.setFont(new Font("MV Boli", Font.BOLD,35));
+        buttonA.setFocusable(false);
+        buttonA.addActionListener(this);
+        buttonA.setText("A");
+
+        buttonB.setBounds(0,200,100,100);
+        buttonB.setFont(new Font("MV Boli", Font.BOLD,35));
+        buttonB.setFocusable(false);
+        buttonB.addActionListener(this);
+        buttonB.setText("B");
+
+        buttonC.setBounds(0,300,100,100);
+        buttonC.setFont(new Font("MV Boli", Font.BOLD,35));
+        buttonC.setFocusable(false);
+        buttonC.addActionListener(this);
+        buttonC.setText("C");
+
+        buttonD.setBounds(0,400,100,100);
+        buttonD.setFont(new Font("MV Boli", Font.BOLD,35));
+        buttonD.setFocusable(false);
+        buttonD.addActionListener(this);
+        buttonD.setText("D");
+
+
+
+
+        answerLabelA.setBounds(125,100,500,100);
+        answerLabelA.setBackground(new Color(220,223,228));
+        answerLabelA.setForeground(new Color(25,255,0));
+        answerLabelA.setFont(new Font("MV Boli", Font.PLAIN,35));
+        answerLabelA.setText("Test labels");
+
+        answerLabelB.setBounds(125,200,500,100);
+        answerLabelB.setBackground(new Color(220,223,228));
+        answerLabelB.setForeground(new Color(25,255,0));
+        answerLabelB.setFont(new Font("MV Boli", Font.PLAIN,35));
+        answerLabelB.setText("Test labels");
+
+        answerLabelC.setBounds(125,300,500,100);
+        answerLabelC.setBackground(new Color(220,223,228));
+        answerLabelC.setForeground(new Color(25,255,0));
+        answerLabelC.setFont(new Font("MV Boli", Font.PLAIN,35));
+        answerLabelC.setText("Test labels");
+
+        answerLabelD.setBounds(125,400,500,100);
+        answerLabelD.setBackground(new Color(220,223,228));
+        answerLabelD.setForeground(new Color(25,255,0));
+        answerLabelD.setFont(new Font("MV Boli", Font.PLAIN,35));
+        answerLabelD.setText("Test labels");
+
+        frame.add(answerLabelA);
+        frame.add(answerLabelB);
+        frame.add(answerLabelC);
+        frame.add(answerLabelD);
+
+
+        frame.add(buttonA);  // ---------------------------------------//
+        frame.add(buttonB);
+        frame.add(buttonC);
+        frame.add(buttonD);  // --------Adds to the frame ---------------//
+
+        frame.add(textArea);
+        frame.add(textField);  // ---------------------------------------//
+        frame.setVisible(true);
     }
     public void nextQuestion(){
 
     }
-    @Override
-    public void actionPreformed(ActionEvent e) {
 
-    }
     public void displayAnswer() {   // displays the answer
 
     }
     public void results(){ // method for the results
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
 
 
